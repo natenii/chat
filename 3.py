c21b3ce7521748dab2820c90d9e8b64302944a1e
Nate Ni <natengai.nn@gmail.com>
2021-02-21 17:08:08 +0800

    added 3.py

---
+++ 3.py
@@ -0,0 +1,12 @@
+
+lines = []
+with open('3.txt', 'r', encoding='utf-8-sig') as f:
+    for line in f:
+        lines.append(line.strip())
+
+for line in lines:
+    s = line.split(' ')
+    name = s[0][5:]
+    rest = s[1:]
+    print(name)
+    print(rest)
